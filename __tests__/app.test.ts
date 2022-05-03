f6b2f45f83060c266bf54373ec4615c29fd82a82
Allan Almazan <allan.almazan@gmail.com>
2022-05-03 14:43:06 -0700

    Add ts-jest for typescript jest support, rename tests js -> ts

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -1,11 +1,12 @@
-const request = require('supertest')
-import app  from '../src/app'
+import type { Response } from 'supertest'
+import request from 'supertest'
+import app from '../src/app'
 
 describe(`'/' route`, function () {
   it('works', function () {
     return request(app)
       .get('/')
-      .then((res) => {
+      .then((res: Response) => {
         expect(res.statusCode).toBe(200)
         expect(res.type).toEqual('text/html')
         expect(res.text).toEqual('Hello')
@@ -13,14 +14,13 @@ describe(`'/' route`, function () {
   })
 })
 
-
 describe(`'/items' route`, function () {
   const baseUrl = '/items'
 
   it('works', function () {
     return request(app)
       .get(baseUrl)
-      .then((res) => {
+      .then((res: Response) => {
         expect(res.statusCode).toBe(200)
         expect(res.type).toEqual('application/json')
         expect(res.body).toEqual([])
